f82a83d9d4b1746ad99f4890a50c144f24f3c0fb
Thomas Buffard <buffardt@gmail.com>
2020-06-27 13:52:14 -0500

    CMake project setup and intial window render

---
+++ cmake-build-default/lib/glm/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/thomas/Documents/COMP371/lib/glm
+# Build directory: /Users/thomas/Documents/COMP371/cmake-build-default/lib/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
+subdirs("test")
---
+++ cmake-build-default/lib/glm/test/bug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/thomas/Documents/COMP371/lib/glm/test/bug
+# Build directory: /Users/thomas/Documents/COMP371/cmake-build-default/lib/glm/test/bug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
