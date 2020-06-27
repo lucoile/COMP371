de3a946a66deb8ff5af00fe00d63759157882d13
Narra Pangan <narrapangan@MacBook-Pro.local>
2020-06-27 17:38:16 -0400

    Added glad package.

---
+++ cmake-build-debug/lib/glad/CMakeFiles/glad-generate-files.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../glad-master/include/glad/glad.h"
+  "../glad-master/src/glad.c"
+  "CMakeFiles/glad-generate-files"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/glad-generate-files.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/lib/glad/CMakeFiles/glad.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../glad-master/include/glad/glad.h"
+  "../glad-master/src/glad.c"
+  "CMakeFiles/glad.dir/__/glad-master/src/glad.c.o"
+  "libglad.a"
+  "libglad.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/glad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
