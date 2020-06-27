043f8f643c35b1c4d117815eb5fd5f40a21d329f
Thomas Buffard <buffardt@gmail.com>
2020-06-27 16:51:58 -0500

    Removed CMake auto-generated folders

--- cmake-build-default/lib/glm/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/thomas/Documents/COMP371/lib/glm
-# Build directory: /Users/thomas/Documents/COMP371/cmake-build-default/lib/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
-subdirs("test")
--- cmake-build-default/lib/glm/test/gtx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/thomas/Documents/COMP371/lib/glm/test/gtx
-# Build directory: /Users/thomas/Documents/COMP371/cmake-build-default/lib/glm/test/gtx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
