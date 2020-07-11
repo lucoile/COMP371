3343ec2ae647f76b4605c3ea0a6f588854d70342
Thomas Buffard <buffardt@gmail.com>
2020-07-10 21:59:35 -0500

    Mesh class

---
+++ src/mesh.h
@@ -0,0 +1,27 @@
+//
+// Created by Thomas Buffard on 7/10/20.
+//
+
+#ifndef COMP371_MESH_H
+#define COMP371_MESH_H
+
+#include <vector>
+#include "shader_m.h"
+
+class Mesh {
+public:
+    std::vector<float> vertices;
+    std::vector<unsigned int> indices;
+
+    Mesh(std::vector<float> vertices, std::vector<unsigned int> indices);
+
+    void draw(Shader &shader);
+
+private:
+    unsigned int VBO, VAO, EBO;
+
+    void setupMesh();
+};
+
+
+#endif //COMP371_MESH_H
