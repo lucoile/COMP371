04d7b3f6e4f3c70a05b02d6289532655569c4c30
Thomas Buffard <buffardt@gmail.com>
2020-08-07 15:08:50 -0500

    Updated terrain class to use FastNoiseSIMD library instead of libnoise
    FastNoiseSIMD allows for more noise types, is faster, and more up to date.

--- src/Utilities/terrain.cpp
+++ src/Utilities/terrain.cpp
@@ -19,46 +19,86 @@ Terrain::Terrain(unsigned int size, Model cube, int octaveCount, float frequency
 
 void Terrain::Render(Shader& shader, glm::mat4 world, glm::vec2 worldPos)
 {
-	int startX =worldPos.x + round(size / 2);
+	int startX = worldPos.x + round(size / 2);
 	int endX = renderSize + worldPos.x + round(size / 2);
 	int startZ = worldPos.y + round(size / 2);
 	int endZ = renderSize + worldPos.y + round(size / 2);
 
 	// Loop through height map and render each voxel
-	for ( int x = startX; x < endX; x++) {
-		for ( int z = startZ; z < endZ; z++) {
-			// Get height at x, z position from height map
-			float height = (heightMap.GetValue(x, z) / 2.0) + 0.5;
+//	for (int x = startX; x < endX; x++) {
+//		for (int z = startZ; z < endZ; z++) {
+//			// Get height at x, z position from height map
+//			float height = (heightMap.GetValue(x, z) / 2.0) + 0.5;
+//
+//			// Calculate position of voxel on x-z plane
+//			glm::vec3 voxelPos((float) (x - (renderSize / 2.0) - startX) * ULEN,
+//					0.0f,
+//					(float) (z - (renderSize  / 2.0) - startZ) * ULEN);
+//
+//			// Scale each voxel to the height in height map rounded to the nearest unit
+//			glm::mat4 model = world *
+//							  glm::scale(id, glm::vec3(1.0f, 1.0 + round((10.0 * height)), 1.0f)) *
+//							  glm::translate(id, voxelPos);
+//
+//			// Render cube with position and height
+//			shader.setMat4("model", model);
+//			cube.Draw(shader, GL_TRIANGLES);
+//		}
+//	}
 
-			// Calculate position of voxel on x-z plane
-			glm::vec3 voxelPos((float) (x - (renderSize / 2.0) - startX) * ULEN,
-					0.0f,
-					(float) (z - (renderSize  / 2.0) - startZ) * ULEN);
+  for (int x = startX; x < endX; x++) {
+	  for (int z = startZ; z < endZ; z++) {
+		  // Get height at x, z position from height map
+//		  std::cout << (heightMapArray)[x * z] << "\n";
+		  float height = ((heightMapArray)[x * z] / 2.0) + 0.5;
 
-			// Scale each voxel to the height in height map rounded to the nearest unit
-			glm::mat4 model = world *
-							  glm::scale(id, glm::vec3(1.0f, 1.0 + round((10.0 * height)), 1.0f)) *
-							  glm::translate(id, voxelPos);
+		  // Calculate position of voxel on x-z plane
+		  glm::vec3 voxelPos((float) (x - (renderSize / 2.0) - startX) * ULEN,
+							 0.0f,
+							 (float) (z - (renderSize  / 2.0) - startZ) * ULEN);
 
-			// Render cube with position and height
-			shader.setMat4("model", model);
-			cube.Draw(shader, GL_TRIANGLES);
+		  // Scale each voxel to the height in height map rounded to the nearest unit
+		  glm::mat4 model = world *
+							glm::scale(id, glm::vec3(1.0f, 1.0 + round((10.0 * height)), 1.0f)) *
+							glm::translate(id, voxelPos);
+
+		  // Render cube with position and height
+		  shader.setMat4("model", model);
+		  cube.Draw(shader, GL_TRIANGLES);
 		}
 	}
 }
 
 void Terrain::genHeightMap()
 {
+	// libnoise Perlin height map generator
 	// Set noise parameters
-	PerlinGen.SetOctaveCount(octaveCount);
-	PerlinGen.SetFrequency(frequency);
-	PerlinGen.SetPersistence(persistence);
+//	PerlinGen.SetOctaveCount(octaveCount);
+//	PerlinGen.SetFrequency(frequency);
+//	PerlinGen.SetPersistence(persistence);
+//
+//	// Generate terrain height map using Perlin noise
+//	utils::NoiseMapBuilderPlane heightMapBuilder;
+//	heightMapBuilder.SetSourceModule(PerlinGen);
+//	heightMapBuilder.SetDestNoiseMap(heightMap);
+//	heightMapBuilder.SetDestSize(size, size);
+//	heightMapBuilder.SetBounds(0.0, 5.0, 0.0, 5.0);
+//	heightMapBuilder.Build();
 
-	// Generate terrain height map using Perlin noise
-	utils::NoiseMapBuilderPlane heightMapBuilder;
-	heightMapBuilder.SetSourceModule(PerlinGen);
-	heightMapBuilder.SetDestNoiseMap(heightMap);
-	heightMapBuilder.SetDestSize(size, size);
-	heightMapBuilder.SetBounds(0.0, 5.0, 0.0, 5.0);
-	heightMapBuilder.Build();
+
+	// FastNoiseSIMD Simplex noise height map generator
+	FastNoiseSIMD* noiseGen = FastNoiseSIMD::NewFastNoiseSIMD();
+	heightMapArray = noiseGen->GetSimplexFractalSet(0, 0, 0, size, size, 1);
 }
+void Terrain::Free ()
+{
+	FastNoiseSIMD::FreeNoiseSet(heightMapArray);
+}
+
+
+
+
+
+
+
+
